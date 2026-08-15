1951e4711e8f92f91e6e9cc2f5d82957005fe62a
Ashish <ashishjain195@gmail.com>
2026-08-15 22:51:14 +0530

    Day 01 - Python baiscs

---
+++ day01/day1.py
@@ -0,0 +1,37 @@
+print("Hello, Python!")
+name="Ashu"
+print(name)
+name="Ganveer"
+print(name)
+age=40
+price=99.50
+print(type(name))
+print(type(age))
+print(type(price))
+a=10
+b=5
+print(a*b)
+print(a/b)
+print(a+b)
+print(a-b)
+print("My name is",name)
+print("My age is",age)
+print(f"My name is {name} and my age is {age}")
+name="Ashish"
+age=40
+city="Mumbai"
+profession="IT Engineer"
+experience=15.5
+print(f"Hi, My name is {name} and I am {age} years old. I have been working in {city} for last {experience} years as {profession}")
+print(type(city))
+print(type(age))
+print(type(experience))
+current_year=2026
+birth_year=1987
+calculated_age=current_year-birth_year
+print(f"My calculated birth year is {calculated_age}")
+product="Notebook"
+price=50.00
+quantity=3
+total=price*quantity
+print(f"I bought {quantity} {product} at ₹{price} each. Total is ₹{total}")
